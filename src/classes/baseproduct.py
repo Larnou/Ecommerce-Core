5194bc3d461a34965119fbe3fb8cf7cdc18145eb
Larnou <sfas921@yandex.ru>
2025-07-31 10:06:51 +0800

    162_2: Обновлён файл README.md, добавлены докстринги

--- src/classes/baseproduct.py
+++ src/classes/baseproduct.py
@@ -2,13 +2,28 @@ from abc import ABC, abstractmethod
 
 
 class BaseProduct(ABC):
+    """
+    Класс BaseProduct, содержит в себе обязательные для реализации методы в классах потомках.
+    """
 
     @property
     @abstractmethod
     def price(self) -> float:
+        """
+        Возвращает цену проудкта.
+
+        Returns:
+            Цена продукта.
+        """
         pass
 
     @price.setter
     @abstractmethod
     def price(self, value: float):
+        """
+        Задаёт новую цену товара
+
+        Args:
+            value: Новая цена.
+        """
         pass
--- src/classes/reprmixin.py
+++ src/classes/reprmixin.py
@@ -2,8 +2,22 @@ from abc import ABC
 
 
 class ReprMixin(ABC):
+    """
+    Класс-миксин ReprMixin, реализует вывод информации при создании экзмляра класса наследника.
+
+    Attributes:
+        *args: Неименованные атрибуты.
+        **kwargs: Именованные атрибуты.
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Создаёт миксин ReprMixin.
+
+        Attributes:
+            *args: Неименованные атрибуты.
+            **kwargs: Именованные атрибуты.
+        """
         args_repr = [repr(a) for a in args]
         kwargs_repr = [f"{k}={repr(v)}" for k, v in kwargs.items()]
         all_args = ", ".join(args_repr + kwargs_repr)
